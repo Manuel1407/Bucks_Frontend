4926457d6276c55e85c3489ffd5742d78c0ca081
Ikechukwu Iwunna <89745907+Manuel1407@users.noreply.github.com>
2023-05-30 08:36:38 +0100

    initial code setup

---
+++ src/test/java/com/ikechukwu/paysensei/PaySenseiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikechukwu.paysensei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaySenseiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
